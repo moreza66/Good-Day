578fee59978378911f97e9aab50afeb300629cf3
mohammadreza memar <moreza.memar@gmail.com>
2023-03-26 20:08:08 -0500

    create a signin Controllers

---
+++ server/controllers/user.js
@@ -0,0 +1,35 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import User from "../models/user.js";
+
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser)
+      return res.status(404).json({ message: "User Doesn't Exist!" });
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      "test",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ result: existingUser, token });
+  } catch (error) {
+    res.status(500).json({ message: "Somthing Went Wrong" });
+  }
+};
+
+export const signup = async (req, res) => {};
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 // const CONNECTION_URL =
 //   "mongodb+srv://mongo:17572031843Lham@cluster0.m7nzp2l.mongodb.net/?retryWrites=true&w=majority";
 
